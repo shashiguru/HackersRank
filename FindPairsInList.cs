071fcd4d0d2077b8f22e68ce69ecf868b346045e
shashiguru <30490543+shashiguru@users.noreply.github.com>
2019-05-28 23:36:59 +0530

    Add files via upload

---
+++ FindPairsInList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackersRank
+{
+   public class FindPairsInList
+    {
+        
+        
+        public int FindPairs(int n, int[] ar)
+        {
+            int result = 0;
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            foreach (int item in ar)
+            {
+                if (!dict.ContainsKey(item))
+                {
+                    dict.Add(item,0);
+                }
+            }
+            foreach (int key in dict.Keys)
+            {
+                int count = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    
+                    if (key==ar[i])
+                    {
+                        count++;
+                    }
+                }
+                result =result+ count / 2;
+                    
+            }
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HackersRank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FindPairsInList fa = new FindPairsInList();
+            Console.WriteLine(fa.FindPairs(9, new int[] { 10, 20 ,20, 10 ,10 ,30 ,50, 10, 20 }));
+            Console.Read();
+        }
+    }
+}
